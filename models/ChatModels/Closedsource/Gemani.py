0bbe528409ad20a2bf749abc59a40a5ae3cb4184
Muhammmad Zaid <muhammad2005zaidkhan@gmail.com>
2026-08-08 23:47:54 +0500

    Create Gemani.py

---
+++ models/ChatModels/Closedsource/Gemani.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro')
+
+result = model.invoke('What is the capital of India')
+
+print(result.content)
