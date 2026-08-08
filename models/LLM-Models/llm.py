4a1a71e2d5cd1a0a8155a65d84661899e704f858
Muhammmad Zaid <muhammad2005zaidkhan@gmail.com>
2026-08-08 23:53:05 +0500

    Update llm.py

--- models/LLM-Models/llm.py
+++ models/LLM-Models/llm.py
@@ -1,1 +1,11 @@
 
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
+
+result = llm.invoke("What is the capital of India")
+
+print(result)
