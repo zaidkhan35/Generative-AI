84b1aae7f706b5d4f54b9bf3ef75f504128e07a1
Muhammmad Zaid <muhammad2005zaidkhan@gmail.com>
2026-08-11 23:05:13 +0500

    Implement RunnableSequence for joke generation

---
+++ Langchain_Chains/Runnables/Runnable_sequential.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({'topic':'AI'}))
